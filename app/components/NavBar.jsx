a93deb59a88ae07cf8475f1067a1e91d868c0b28
gerardjr42 <ggarciajr12@gmail.com>
2024-08-16 05:03:11 -0400

    create navbar component

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="p-4 bg-gray-950 bg-opacity-60 flex justify-between items-center">
+      <p className="text-white text-2xl font-bold">Serenify</p>
+      <ul className="flex items-center">
+        <li className="mr-4 ">
+          <Link href="/about" className="text-white hover:text-gray-200">
+            About
+          </Link>
+        </li>
+        <li className="mr-4">
+          <Link href="/chat" className="text-white hover:text-gray-200">
+            Chat with Serenify
+          </Link>
+        </li>
+        <li className="mr-4">
+          <Link href="/login" className="text-white hover:text-gray-200">
+            <button className="bg-white text-black px-2 py-1 rounded-md">
+              Login/Logout
+            </button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
 "use client";
-import { Box, Button, Stack, TextField } from "@mui/material";
-import Spline from "@splinetool/react-spline/next";
 import { useState } from "react";
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   const [message, setMessage] = useState("");
@@ -62,111 +59,117 @@ export default function Home() {
   };
 
   return (
-    <>
-      <div>
-        <Spline scene="https://prod.spline.design/6nHbXNojOn8UGSpN/scene.splinecode" />
-      </div>
-      <Box
-        width="100vw"
-        height="100vh"
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Box>
-          <h1>Serenify</h1>
-        </Box>
-        <Stack
-          direction="column"
-          spacing={3}
-          width="600px"
-          height="700px"
-          border="1px solid #ccc"
-          p={2}
-        >
-          <Stack
-            direction="column"
-            p={2}
-            spacing={2}
-            flexGrow={1}
-            overflow="auto"
-            maxHeight="100%"
-            // width="100%"
-          >
-            {messages.map((message, index) => (
-              <Box
-                key={index}
-                display="flex"
-                justifyContent={
-                  message.role === "assistant" ? "flex-start" : "flex-end"
-                }
-                width="100%"
-                mb={2}
-              >
-                <Box
-                  bgcolor={
-                    message.role === "assistant"
-                      ? "primary.main"
-                      : "secondary.main"
-                  }
-                  p={2}
-                  borderRadius={2}
-                  color="white"
-                  maxWidth="80%"
-                >
-                  <ReactMarkdown
-                    remarkPlugins={[remarkGfm]}
-                    components={{
-                      h1: ({ node, ...props }) => (
-                        <h1
-                          className="mt-4 mb-2 text-xl font-bold"
-                          {...props}
-                        />
-                      ),
-                      h2: ({ node, ...props }) => (
-                        <h2
-                          className="mt-4 mb-2 text-lg font-semibold"
-                          {...props}
-                        />
-                      ),
-                      p: ({ node, ...props }) => (
-                        <p className="mb-2" {...props} />
-                      ),
-                      ul: ({ node, ...props }) => (
-                        <ul className="mb-2 ml-6 list-disc" {...props} />
-                      ),
-                      ol: ({ node, ...props }) => (
-                        <ol className="mb-2 ml-6 list-decimal" {...props} />
-                      ),
-                      li: ({ node, ...props }) => (
-                        <li className="mb-1" {...props} />
-                      ),
-                    }}
-                    className="markdown-content"
-                  >
-                    {message.content}
-                  </ReactMarkdown>
-                </Box>
-              </Box>
-            ))}
-          </Stack>
-          <Stack direction="row" spacing={2}>
-            <TextField
-              label="message"
-              fullWidth
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              onKeyDown={(e) =>
-                e.key === "Enter" && !e.shiftKey && handleSendMessage()
-              }
-            />
-            <Button variant="contained" onClick={handleSendMessage}>
-              Send
-            </Button>
-          </Stack>
-        </Stack>
-      </Box>
-    </>
+    // <>
+    //   <div>
+    //     <Spline scene="https://prod.spline.design/6nHbXNojOn8UGSpN/scene.splinecode" />
+    //   </div>
+    //   <Box
+    //     width="100vw"
+    //     height="100vh"
+    //     display="flex"
+    //     flexDirection="column"
+    //     justifyContent="center"
+    //     alignItems="center"
+    //   >
+    //     <Box>
+    //       <h1>Serenify</h1>
+    //     </Box>
+    //     <Stack
+    //       direction="column"
+    //       spacing={3}
+    //       width="600px"
+    //       height="700px"
+    //       border="1px solid #ccc"
+    //       p={2}
+    //     >
+    //       <Stack
+    //         direction="column"
+    //         p={2}
+    //         spacing={2}
+    //         flexGrow={1}
+    //         overflow="auto"
+    //         maxHeight="100%"
+    //         // width="100%"
+    //       >
+    //         {messages.map((message, index) => (
+    //           <Box
+    //             key={index}
+    //             display="flex"
+    //             justifyContent={
+    //               message.role === "assistant" ? "flex-start" : "flex-end"
+    //             }
+    //             width="100%"
+    //             mb={2}
+    //           >
+    //             <Box
+    //               bgcolor={
+    //                 message.role === "assistant"
+    //                   ? "primary.main"
+    //                   : "secondary.main"
+    //               }
+    //               p={2}
+    //               borderRadius={2}
+    //               color="white"
+    //               maxWidth="80%"
+    //             >
+    //               <ReactMarkdown
+    //                 remarkPlugins={[remarkGfm]}
+    //                 components={{
+    //                   h1: ({ node, ...props }) => (
+    //                     <h1
+    //                       className="mt-4 mb-2 text-xl font-bold"
+    //                       {...props}
+    //                     />
+    //                   ),
+    //                   h2: ({ node, ...props }) => (
+    //                     <h2
+    //                       className="mt-4 mb-2 text-lg font-semibold"
+    //                       {...props}
+    //                     />
+    //                   ),
+    //                   p: ({ node, ...props }) => (
+    //                     <p className="mb-2" {...props} />
+    //                   ),
+    //                   ul: ({ node, ...props }) => (
+    //                     <ul className="mb-2 ml-6 list-disc" {...props} />
+    //                   ),
+    //                   ol: ({ node, ...props }) => (
+    //                     <ol className="mb-2 ml-6 list-decimal" {...props} />
+    //                   ),
+    //                   li: ({ node, ...props }) => (
+    //                     <li className="mb-1" {...props} />
+    //                   ),
+    //                 }}
+    //                 className="markdown-content"
+    //               >
+    //                 {message.content}
+    //               </ReactMarkdown>
+    //             </Box>
+    //           </Box>
+    //         ))}
+    //       </Stack>
+    //       <Stack direction="row" spacing={2}>
+    //         <TextField
+    //           label="message"
+    //           fullWidth
+    //           value={message}
+    //           onChange={(e) => setMessage(e.target.value)}
+    //           onKeyDown={(e) =>
+    //             e.key === "Enter" && !e.shiftKey && handleSendMessage()
+    //           }
+    //         />
+    //         <Button variant="contained" onClick={handleSendMessage}>
+    //           Send
+    //         </Button>
+    //       </Stack>
+    //     </Stack>
+    //   </Box>
+    // </>
+    <div className="bg-black min-h-screen">
+      <NavBar />
+      <main>
+        <h1>Welcome to Serenify</h1>
+      </main>
+    </div>
   );
 }
